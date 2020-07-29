7691e747650630ea56120273deecb8381e6db2bc
Gil Finkelstein <zoldan.rafael@gmail.com>
2020-07-29 14:49:28 +0300

    ios ffmpeg implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:googly_eyes/pages/addVoice.dart';
+import 'package:googly_eyes/pages/fbShare.dart';
 import 'package:googly_eyes/pages/home.dart';
 import 'package:googly_eyes/pages/makeImage.dart';
 
 void main() => runApp(MaterialApp(initialRoute: '/home', routes: {
       '/home': (context) => Home(),
       '/image': (context) => MakeImage(),
-      '/edit': (context) => AddVoice(),
+      '/voice': (context) => AddVoice(),
+      '/fbshare': (context) => FbShare(),
     }));
