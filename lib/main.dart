589c45f7c954b163804afecf40525e3fee52d45a
Rafa Zoldan <zoldan.rafael@gmail.com>
2020-08-13 01:39:39 +0300

    block rotate

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:googly_eyes/pages/addVoice.dart';
 import 'package:googly_eyes/pages/fbShare.dart';
 import 'package:googly_eyes/pages/home.dart';
 import 'package:googly_eyes/pages/makeImage.dart';
 import 'package:googly_eyes/pages/start.dart';
 
-void main() => runApp(MaterialApp(initialRoute: '/start', routes: {
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    print('hello');
+    runApp(MaterialApp(initialRoute: '/start', routes: {
       '/start': (context) => Start(),
       '/home': (context) => Home(),
       '/image': (context) => MakeImage(),
       '/voice': (context) => AddVoice(),
       '/fbshare': (context) => VideoApp(),
     }));
+  });
+}
