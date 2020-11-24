5b51917ae9bc767c8ea2b75075850b9a0b384549
Rafa Zoldan <zoldan.rafael@gmail.com>
2020-11-24 09:15:48 +0200

    :heavy_plus_sign: adding files based on vc gitignore

---
+++ android/app/src/main/kotlin/com/example/googly_eyes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.looneycam.looneycam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bloc/singleton_bloc.dart
@@ -0,0 +1,15 @@
+// import 'dart:async';
+// import 'package:googly_eyes/utilities/recordSound.dart';
+
+// class BlockProvider {
+//   final pathStream = StreamController<String>();
+
+//   static BlockProvider _instance;
+
+//   BlockProvider._();
+
+//   static BlockProvider get getIstance =>
+//       _instance = _instance ?? BlockProvider._();
+
+//   void record() async {}
+// }
